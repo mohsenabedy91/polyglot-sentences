ac964077a0a65b84345d2692e2f82f9a4207a977
mohsen-abedy <mohsen.abedy@yahoo.com>
2024-06-25 13:14:10 +0330

    fix naming convention

--- pkg/claim/gin_test.go
+++ pkg/claim/gin_test.go
@@ -11,42 +11,42 @@ import (
 
 func TestGetUserUUIDFromGinContext(t *testing.T) {
 	tests := []struct {
-		name           string
-		userUUIDStr    string
-		expectedResult uuid.UUID
-		expectedError  bool
-		contextSet     bool
+		name             string
+		userUUIDStr      string
+		expectedResult   uuid.UUID
+		expectedError    bool
+		shouldSetContext bool
 	}{
 		{
-			name:           "Valid UUID in context",
-			userUUIDStr:    "123e4567-e89b-12d3-a456-426614174000",
-			expectedResult: uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"),
-			expectedError:  false,
-			contextSet:     true,
+			name:             "Valid UUID in context",
+			userUUIDStr:      "123e4567-e89b-12d3-a456-426614174000",
+			expectedResult:   uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"),
+			expectedError:    false,
+			shouldSetContext: true,
 		},
 		{
-			name:          "Invalid UUID in context",
-			userUUIDStr:   "invalid-uuid",
-			expectedError: true,
-			contextSet:    true,
+			name:             "Invalid UUID in context",
+			userUUIDStr:      "invalid-uuid",
+			expectedError:    true,
+			shouldSetContext: true,
 		},
 		{
-			name:          "Missing UUID in context",
-			userUUIDStr:   "",
-			expectedError: true,
-			contextSet:    true,
+			name:             "Missing UUID in context",
+			userUUIDStr:      "",
+			expectedError:    true,
+			shouldSetContext: true,
 		},
 		{
-			name:          "Missing set context",
-			expectedError: true,
-			contextSet:    false,
+			name:             "Missing set context",
+			expectedError:    true,
+			shouldSetContext: false,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ctx := &gin.Context{}
-			if test.contextSet {
+			if test.shouldSetContext {
 				ctx.Set(config.AuthTokenUserUUID, test.userUUIDStr)
 			}
 
@@ -64,33 +64,33 @@ func TestGetUserUUIDFromGinContext(t *testing.T) {
 
 func TestGetJTIFromGinContext(t *testing.T) {
 	tests := []struct {
-		name           string
-		jti            string
-		expectedResult string
-		contextSet     bool
+		name             string
+		jti              string
+		expectedResult   string
+		shouldSetContext bool
 	}{
 		{
-			name:           "Valid JTI in context",
-			jti:            "some-jti-value",
-			expectedResult: "some-jti-value",
-			contextSet:     true,
+			name:             "Valid JTI in context",
+			jti:              "some-jti-value",
+			expectedResult:   "some-jti-value",
+			shouldSetContext: true,
 		},
 		{
-			name:           "Missing JTI in context",
-			jti:            "",
-			expectedResult: "",
-			contextSet:     true,
+			name:             "Missing JTI in context",
+			jti:              "",
+			expectedResult:   "",
+			shouldSetContext: true,
 		},
 		{
-			name:       "Missing set context",
-			contextSet: false,
+			name:             "Missing set context",
+			shouldSetContext: false,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ctx := &gin.Context{}
-			if test.contextSet {
+			if test.shouldSetContext {
 				ctx.Set(config.AuthTokenJTI, test.jti)
 			}
 
@@ -102,33 +102,33 @@ func TestGetJTIFromGinContext(t *testing.T) {
 
 func TestGetExpFromGinContext(t *testing.T) {
 	tests := []struct {
-		name           string
-		expirationTime float64
-		expectedResult int64
-		contextSet     bool
+		name             string
+		expirationTime   float64
+		expectedResult   int64
+		shouldSetContext bool
 	}{
 		{
-			name:           "Valid expiration time in context",
-			expirationTime: 1627840201.0,
-			expectedResult: 1627840201,
-			contextSet:     true,
+			name:             "Valid expiration time in context",
+			expirationTime:   1627840201.0,
+			expectedResult:   1627840201,
+			shouldSetContext: true,
 		},
 		{
-			name:           "Missing expiration time in context",
-			expirationTime: 0,
-			expectedResult: 0,
-			contextSet:     true,
+			name:             "Missing expiration time in context",
+			expirationTime:   0,
+			expectedResult:   0,
+			shouldSetContext: true,
 		},
 		{
-			name:       "Missing set context",
-			contextSet: false,
+			name:             "Missing set context",
+			shouldSetContext: false,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ctx := &gin.Context{}
-			if test.contextSet {
+			if test.shouldSetContext {
 				ctx.Set(config.AuthTokenExpirationTime, test.expirationTime)
 			}
 
